ce7c7e5a9827f46da69b35203fbfcee4251ddf31
razorblade23 <81590614+razorblade23@users.noreply.github.com>
2025-08-14 18:57:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cowsay
 
 message = """
 The most remarkable thing about my mother is that for thirty years she served
-the family nothing but leftovers.  The original meal has never been found.
+the family nothing but leftovers. Lets test this.  The original meal has never been found.
         -- Calvin Trillin
 """.strip()
 
